2dade274212fcac19eea8e63fbb1621dfadad3c6
Annabelle Lee <pinkannabelle@gmail.com>
2024-08-17 15:51:22 -0700

    added chicken animation

--- scripts/chicken.gd
+++ scripts/chicken.gd
@@ -7,12 +7,17 @@ extends CharacterBody2D
 
 @onready var main = get_tree().get_root().get_node("game")
 @onready var projectile = load("res://scenes/egg.tscn")
+@onready var anim_sprite = $AnimatedSprite2D
+
 
 #chicken
 const SPEED = 10000.0
 var can_lay = true
+var has_clicked = false
 var growth_rate = 0.5
 var power = 0
+var size = 1
+var just_laid_egg = false
 #egg
 var charging = false
 var charge_time = 0.0
@@ -20,28 +25,62 @@ var max_charge_time = 2.0 # Time in seconds to fully charge
 
 func _ready():
 	velo = 0
-	pass
+	
 #Shoot Egg
 func _input(event):
 	if Input.is_action_just_pressed("click") && can_lay:
 		print("charging")
 		charging = true
+		has_clicked = true
 		charge_time = 0.0
-	elif Input.is_action_just_released("click") && can_lay:
+	
+	elif Input.is_action_just_released("click") && can_lay && has_clicked:
 		can_lay = false
-		print("hello mommy")
+		has_clicked = false
 		charging = false
 		$Cooldown.start()
-		shoot_projectile()        
+		shoot_projectile()
+		just_laid_egg = true
+		
+		
+	
+		   
 
 
 
 func _process(delta):
 	if charging:
+		## loading the animations, as of rn animations for med still needed
+		if size >=1 && size < 5:
+			if charge_time >= 2: 
+				anim_sprite.play("small_max")
+			else: 
+				anim_sprite.play("small_lay")
+		elif size >= 5 && size < 10:
+			if charge_time >= 2: 
+				anim_sprite.play("fat_max")
+			else: 
+				anim_sprite.play("fat_lay")
+		elif size >= 10: 
+			if charge_time >= 2: 
+				anim_sprite.play("fat_max")
+			else: 
+				anim_sprite.play("fat_lay")
 		charge_time += delta
 		charge_time = clamp(charge_time, 0, max_charge_time)
+		
+		power = exp(charge_time)
+	else: 
+		if size >=1 && size < 5:
+			anim_sprite.play("small_idle")	
+		elif size >= 5 && size < 10:
+			anim_sprite.play("med_idle")
+		elif size >= 10: 
+			anim_sprite.play("fat_idle")
+		
 	
-	power = exp(charge_time)
+
+		
 	
 	
 
@@ -54,6 +93,7 @@ func shoot_projectile():
 	print("Power: ",charge_ratio)
 	instance.scale = Vector2(1 + charge_ratio, 1 + charge_ratio)
 	main.add_child(instance)
+	 
 	
  #Physics for chicken
 func _physics_process(delta): 
@@ -61,14 +101,17 @@ func _physics_process(delta):
 	rotation = lerp_angle(rotation, dir.angle()+PI/2, 5 * delta)
 	
 	
-	if Input.is_action_just_released("click"):
+	if Input.is_action_just_released("click") && just_laid_egg:
+		just_laid_egg = false
 		velocity +=  dir * SPEED * delta * power
+	
 	position += velocity * delta
 	velo = velocity.length()
 	var temp = rotation * 180 / PI
 	angle = int(temp) % 360
 	if angle < 0:
 		angle = 360 + angle
+	
 
 func getVelo():
 	return velo
