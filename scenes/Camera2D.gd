d27b9b6866d7a9eb5b1ddcfb5a208ee09fc6d4b1
airwuu <1kookyw1@gmail.com>
2024-08-18 07:50:44 -0700

    the title

--- scenes/Camera2D.gd
+++ scenes/Camera2D.gd
@@ -2,7 +2,7 @@
 extends Camera2D
 var chicken
 var base = 0.5
-var smooth_zoom = 0.0
+var smooth_zoom = 0.5
 var desiredFov: float
 var target_zoom: float
 # Called when the node enters the scene tree for the first time.
@@ -14,4 +14,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	target_zoom = 1.0 / (pow((chicken.mass / 10.0), 1.0/3.0))
-	zoom = Vector2(target_zoom, target_zoom)
+	#zoom = Vector2(target_zoom, target_zoom)
+	smooth_zoom = lerp(smooth_zoom, target_zoom, 5*delta)
+	zoom = Vector2(smooth_zoom, smooth_zoom)
