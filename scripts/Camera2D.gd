b6831624a9cbc3b1ada214e5543348b01aad3230
airwuu <1kookyw1@gmail.com>
2024-08-18 20:09:27 -0700

    one line to set initial zoom

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -4,11 +4,11 @@ var chicken
 var base = 0.5
 var smooth_zoom = 0.5
 var desiredFov: float
-var target_zoom: float
+var target_zoom: float 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	chicken = get_tree().get_root().get_node("game").get_node("Chicken")
-
+	smooth_zoom = base / (pow((chicken.mass / 10.0), 1.0/3.0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
