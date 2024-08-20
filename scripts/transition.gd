ed3566250e7211ec5c6e276bdfdca63e45e24e29
BetaBear2 <90876300+BetaBear2@users.noreply.github.com>
2024-08-19 17:32:34 -0700

    skip button works all the time

--- scripts/transition.gd
+++ scripts/transition.gd
@@ -16,6 +16,8 @@ func transition(anim_name):
 
 func _on_animation_player_animation_finished(anim_name):
 	on_transition_finished.emit()
+	if anim_name == "fade_to_normal":
+		color_rect.visible = false
 	#if anim_name == "fade_to_black":
 		#anim_player.play("fade_to_normal")
 		#on_transition_finished.emit()
