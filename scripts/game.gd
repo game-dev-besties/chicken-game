9179107687e0409a0201803f7ece166f5929942b
BetaBear2 <90876300+BetaBear2@users.noreply.github.com>
2024-08-20 04:03:21 -0700

    added pause menu

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,26 @@
 extends Node2D
 
+@onready var pause_menu = $"hud nightmare/HUD/pauseMenu"
+var paused = false
+
 func _ready():
+	pause_menu.hide()
+	pause_menu.showing = false
 	$transition.transition("fade_to_normal")
 	Global.init_stats()
+
+
+func _input(event):
+	if Input.is_action_just_pressed("pause"):
+		paused = !paused
+		pauseMenu()
+
+func pauseMenu():
+	if paused:
+		pause_menu.show()
+		get_tree().paused = true
+	else:
+		pause_menu.hide()
+		get_tree().paused = false
+
+
