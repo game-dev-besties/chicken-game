9839886d8ae7ceace1ac70acfa756d3b3a4cae7d
BetaBear2 <90876300+BetaBear2@users.noreply.github.com>
2024-08-20 04:32:21 -0700

    fixed credits

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -15,7 +15,7 @@ extends Control
 
 func _ready():
 	start_game.button_up.connect(on_start_button_up)
-	credits.button_up.connect(_on_credits_pressed)
+	credits.button_up.connect(_on_credits_button_up)
 	exit.button_up.connect(_on_exit_pressed)
 	print("test")
 	$transition.transition("fade_to_normal")
@@ -25,13 +25,12 @@ func on_start_button_up() -> void:
 	await $transition.on_transition_finished
 	get_tree().change_scene_to_packed(cutscene)
 	#get_tree().change_scene_to_packed(game)
-	
-
-func _on_credits_pressed():
-	$transition.transition("fade_to_black")
-	await $transition.on_transition_finished
-	get_tree().change_scene_to_packed(credit_scene)
-
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+
+func _on_credits_button_up():
+	$transition.transition("fade_to_black")
+	await $transition.on_transition_finished
+	get_tree().change_scene_to_packed(credit_scene)
